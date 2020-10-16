51e9d0daa72941d8a406a1df41799e5b2b761ad9
jschmitt <jasonschmittcode@gmail.com>
2020-10-16 19:21:19 -0400

    splitting out a few scss files into their individual components

--- client/src/Hero/Hero.js
+++ client/src/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Hero.scss';
 
 function Hero() {
   return (
--- client/src/Menu/Menu.js
+++ client/src/Menu/Menu.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Menu.scss';
+
 import {
   Link
 } from "react-router-dom";
